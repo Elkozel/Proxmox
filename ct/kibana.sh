e6f7844ef3846768dbfe64b5de1b5037c51bcefe
Tsvetomir Hristov <caci802@gmail.com>
2024-10-15 11:49:43 +0200

    Making the end message clearer

--- ct/kibana.sh
+++ ct/kibana.sh
@@ -75,5 +75,7 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
-echo -e "${APP} Setup should be reachable by going to the following URL.
+echo -e "${APP} is installed! For security purposes, ${APP} is only reachable trough localhost. \n
+          To access the ${APP} dashboard, please adjust ${BL}server.host${CL} in ${BL}/etc/kibana/kibana.yml${CL}.
+          After that, ${APP} should bre reachable at
          ${BL}http://${IP}:5601${CL} \n"
